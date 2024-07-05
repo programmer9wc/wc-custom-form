a6c419c4f161e7771be4cb765d045898f121a7f2
programmer9wc <programmer9@webbycrown.com>
2024-07-05 16:03:56 +0530

    add files

---
+++ src/Providers/CustomizedServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Programmer9WC\WcCustomForm\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+
+class CustomizedServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        //
+    }
+    
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    
+
+}
